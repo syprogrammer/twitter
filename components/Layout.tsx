0bb94a89e8ab1e3d8d27ef47dcdf2ad4d099529d
syprogrammer <sonaly647@gmail.com>
2023-09-12 13:56:08 +0530

    removed h-screen from layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ import Sidebar from "@/components/layout/Sidebar"
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="h-screen bg-black">
+    <div className=" bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-[90vw]">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
